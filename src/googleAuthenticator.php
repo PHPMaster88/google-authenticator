03020ad9b3444bab2c4d9bf6a969671e98771c18
John Habila <36119859+PHPMaster88@users.noreply.github.com>
2023-04-12 08:59:12 +0100

    Add files via upload

--- src/googleAuthenticator.php
+++ src/googleAuthenticator.php
@@ -1,1 +1,13 @@
-
+<?php
+    namespace PHPMaster88\authenticator;
+    /**
+     * Google Authenticator
+     * 
+     * (c) John Yusuf Habila <Senestro88@gmail.com>
+     * 
+     * For the full copyright and license information, please view the LICENSE
+     * file that was distributed with this source code.
+     */
+    class googleAuthenticator{
+        
+    }
